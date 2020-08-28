4a35348b9a86d84c0cda4980022e8939bf302314
Sofia Yo <dimitrova.sophiya@gmail.com>
2020-08-28 17:57:17 +0300

    added method @updateStatusToMines()

--- src/model/MaskedBoard.java
+++ src/model/MaskedBoard.java
@@ -13,47 +13,8 @@ public class MaskedBoard extends BoardRepository.GameBoard {
         return super.getBoard();
     }
 
-//    public Cell[][] generateGameBoard(int size, int colX, int rolY, int mines) {
-//
-//        for (int y = 0; y < size; y++) {
-//            for (int x = 0; x < size; x++) {
-////                gameBoard[y][x] = new MaskedCell();
-//            }
-//        }
-//
-//        setGameBoardMines(size, colX, rolY, mines);
-//
-//        return gameBoard;
-//    }
-
-//    public Cell[][] getGameBoard() {
-//        return this.gameBoard;
-//    }
-
-//    public boolean getCellStatus(int r, int c) {
-//        return this.gameBoard[r][c].getIsMine();
-//    }
-
-//    private void setGameBoardMines(int size, int colX, int rolY, int mines) {
-//        int counter = mines;
-//        Random random = new Random();
-//        while (counter > 0) {
-//            int row = random.nextInt(size);
-//            int col = random.nextInt(size);
-//
-//            if (row == rolY && col == colX) {
-//                while (row == rolY || col == colX) {
-//                    row = random.nextInt(size);
-//                    col = random.nextInt(size);
-//                }
-//            }
-//
-//            if (!gameBoard[row][col].hasMine) {
-//                gameBoard[row][col] = new MineCell();
-//                counter--;
-//            }
-//        }
-//    }
+    public void updateStatusToMines(int r, int c){
+            super.getBoard()[r][c] = new MineCell(); }
 
 
 }

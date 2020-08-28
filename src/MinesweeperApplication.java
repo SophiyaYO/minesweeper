cd5b9845eb65571f47c1c0b1594e732451c1ac07
Sofia Yo <dimitrova.sophiya@gmail.com>
2020-08-28 17:56:07 +0300

    simplified

--- src/MinesweeperApplication.java
+++ src/MinesweeperApplication.java
@@ -1,5 +1,4 @@
 import controller.BoardController;
-import view.GameMessage;
 import view.IntroMsg;
 
 import java.io.BufferedReader;
@@ -10,31 +9,32 @@ public class MinesweeperApplication {
     private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
-        IntroMsg msg = new IntroMsg();
-        System.out.println(msg.getIntroMsg());
+        IntroMsg introMsg = new IntroMsg();
+        introMsg.getIntroMsg();
 
         String input = reader.readLine();
         int selectedLevel = Integer.parseInt(input);
 
+        BoardController game = new BoardController(selectedLevel);
+        game.initialStart();
 
         String[] moveCoord = reader.readLine().split(" ");
         int col = Integer.parseInt(moveCoord[1]);
         int rol = Integer.parseInt(moveCoord[0]);
 
-        BoardController game = new BoardController(selectedLevel);
-        game.play(rol,col);
+        game.play(rol, col);
+        game.firstCellChosen(rol,col);
 
         boolean hasLost = false;
 
-//        while (!hasLost){
-//            System.out.println(activeMsg.getMsgMove());
-//            moveCoord = reader.readLine().split(" ");
-//            col = Integer.parseInt(moveCoord[1]);
-//            rol = Integer.parseInt(moveCoord[0]);
-//
-////            gameBoard.play(rol, col);
-////            hasLost = gameBoard.getDead();
-////            System.out.println(activeMsg.getMsgStatus());
-//        }
+        while (!hasLost){
+            moveCoord = reader.readLine().split(" ");
+            col = Integer.parseInt(moveCoord[1]);
+            rol = Integer.parseInt(moveCoord[0]);
+
+            game.play(rol,col);
+            hasLost=game.getDead();
+
+        }
     }
 }
